a84d143a13ec9aa6c7ec2d71be41f1071051df5b
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-03-05 11:41:23 +0100

    Finish listenedEpisodes-services.js functionality

---
+++ src/lib/listenedEpisodes-services.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+require("dotenv").config();
+
+class ListenedEpisode {
+  constructor() {
+    this.listenedEpisode = axios.create({
+      baseURL: `${process.env.BACKEND_DEV}/listenedEpisode`,
+      withCredentials: true
+    });
+  }
+
+  setNewListenedEpisode = async episodeID => {
+    return await this.listenedEpisode.post("/", { episodeID: episodeID });
+  };
+
+  setEpisodeProgress = async (episodeID, progress) => {
+    return await this.listenedEpisode.put(`/${episodeID}`, {
+      progress: progress
+    });
+  };
+}
+
+const listenedEpisodeServices = new ListenedEpisode();
+
+module.exports = listenedEpisodeServices;
